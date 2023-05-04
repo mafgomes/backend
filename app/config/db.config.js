fd485761cbb52e182fa84b4221fa09837b75e5ea
Marcelo A F Gomes <mafgomesinfo@gmail.com>
2023-05-04 05:05:29 +0000

    Usando biblioteca dotenv

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 module.exports = {
-  HOST: "dbHostname",
-  USER: "dbUser",
-  PASSWORD: "dbPassword",
-  DB: "testdb",
+  HOST: process.env.MYSQLDB_USER,
+  USER: process.env.MYSQLDB_HOST,
+  PASSWORD: process.env.MYSQLDB_PASSWORD,
+  DB: process.env.MYSQLDB_DATABASE,
   dialect: "mysql",
   pool: {
     max: 5,
